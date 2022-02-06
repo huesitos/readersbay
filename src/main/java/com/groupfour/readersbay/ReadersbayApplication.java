2c711a4fd6215fe3bd6e913079fc17bb9a4bbcf2
Denisse Lara <dmlaramartin@gmail.com>
2022-02-06 19:03:20 -0400

    Create project using Spring initializ

---
+++ src/main/java/com/groupfour/readersbay/ReadersbayApplication.java
@@ -0,0 +1,13 @@
+package com.groupfour.readersbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadersbayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadersbayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupfour/readersbay/ReadersbayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupfour.readersbay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadersbayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

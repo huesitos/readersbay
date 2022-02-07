19a34e40b92788ad7edfe510c72c760964cc7ac8
jwmenjivar <jwmenjivar@gmail.com>
2022-02-06 18:54:54 -0600

    started rest apis

--- src/main/java/com/groupfour/readersbay/ReadersBayApplication.java
+++ src/main/java/com/groupfour/readersbay/ReadersBayApplication.java
@@ -10,4 +10,5 @@ public class ReadersBayApplication {
 		SpringApplication.run(ReadersBayApplication.class, args);
 	}
 
+
 }
---
+++ src/main/java/com/groupfour/readersbay/service/BookServiceImpl.java
@@ -0,0 +1,10 @@
+package com.groupfour.readersbay.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class BookServiceImpl implements BookService {
+
+}
